eefcf6a7ff315f968b613e88dc6850609fddc7da
sean.hoar <sean.hoar@gmail.com>
2021-10-15 12:44:02 -0400

    Work from cloud ide

---
+++ lessons/Files_bytes_to_file_with_WriteFile/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.OpenFile(
+		"b.txt",
+		os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0644,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	byteSlice := []byte("I learn Golang!")
+	bytesWritten, err := file.Write(byteSlice)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Bytes Written: %d\n", bytesWritten)
+}
